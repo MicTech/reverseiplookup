e32c68f4c73ef848c41a53a2b1171e4e88e597ac
Michal Tehnik <michal.tehnik@mictech.cz>
2014-10-26 13:02:10 +0100

    Add additional stats for preprocess

--- data/preprocess.R
+++ data/preprocess.R
@@ -2,5 +2,13 @@ ip.addresses.ranges <- read.csv('data/partial.csv', header=FALSE, stringsAsFacto
 
 ip.addresses.ranges.transformed <- ip.addresses.ranges[,c(1,2,4)]
 
+from.min <- min(ip.addresses.ranges.transformed[,1])
+from.max <- max(ip.addresses.ranges.transformed[,1])
+
+to.min <- min(ip.addresses.ranges.transformed[,2])
+to.max <- max(ip.addresses.ranges.transformed[,2])
+
+diff <- to.max - from.min
+
 write.csv(ip.addresses.ranges.transformed[1:5,], file="partial.transformed.test.csv", row.names=FALSE, quote=FALSE)
 write.csv(ip.addresses.ranges.transformed, file="partial.transformed.csv", row.names=FALSE, quote=FALSE)
